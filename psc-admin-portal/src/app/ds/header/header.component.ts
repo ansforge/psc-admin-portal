a28b77e692846c4d3bafd6a0b8d010700e26acf5
Eric Degenetais <eric.degenetais.ext@esante.gouv.fr>
2024-04-10 17:40:19 +0200

    Inserting the menu in the header.

--- psc-admin-portal/src/app/ds/header/header.component.ts
+++ psc-admin-portal/src/app/ds/header/header.component.ts
@@ -15,11 +15,12 @@
 ///
 
 import { Component } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ MenuComponent ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
