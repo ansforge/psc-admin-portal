2c6138d1bded85c2ed2b2025c1b94a404b87997c
Eric Degenetais <eric.degenetais.ext@esante.gouv.fr>
2024-05-03 10:55:09 +0200

    Creating the GestionAlertes component.

---
+++ psc-admin-portal/src/app/actions/gestion-alertes/gestion-alertes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-alertes',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-alertes.component.html',
+  styleUrl: './gestion-alertes.component.scss'
+})
+export class GestionAlertesComponent {
+
+}
