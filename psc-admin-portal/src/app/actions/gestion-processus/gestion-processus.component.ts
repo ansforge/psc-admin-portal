359f99a6b8b989ab3367c567616942d2ddb9859d
Eric Degenetais <eric.degenetais.ext@esante.gouv.fr>
2024-05-03 10:52:31 +0200

    Creating the GestionProcessus component.

---
+++ psc-admin-portal/src/app/actions/gestion-processus/gestion-processus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-processus',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-processus.component.html',
+  styleUrl: './gestion-processus.component.scss'
+})
+export class GestionProcessusComponent {
+
+}
