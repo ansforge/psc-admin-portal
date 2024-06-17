e2412b96827155380d97ae95abc148ffb98aae00
lmurat <lmurat@henix.fr>
2024-06-17 10:07:57 +0200

    test interrogation-ps.component.spec.ts OnInit + findPSByIDNat

--- psc-admin-portal/src/app/interrogation-ps/interrogation-ps.component.spec.ts
+++ psc-admin-portal/src/app/interrogation-ps/interrogation-ps.component.spec.ts
@@ -18,23 +18,110 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InterrogationPsComponent } from './interrogation-ps.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {of, Subject} from 'rxjs';
+import {QueryStatusEnum} from '../api/queryStatus.model';
+import {PsApi} from '../api/psApi.service';
 
 describe('InterrogationPsComponent', () => {
   let component: InterrogationPsComponent;
   let fixture: ComponentFixture<InterrogationPsComponent>;
+  let psApiService: jasmine.SpyObj<PsApi>;
 
   beforeEach(async () => {
+    const psApiSpy = jasmine.createSpyObj('PsApi', ['getPSByIDNat']);
+
     await TestBed.configureTestingModule({
-      imports: [InterrogationPsComponent, HttpClientTestingModule]
+      imports: [InterrogationPsComponent, HttpClientTestingModule, ReactiveFormsModule],
+      providers: [
+        { provide: PsApi, useValue: psApiSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(InterrogationPsComponent);
+    psApiService = TestBed.inject(PsApi) as jasmine.SpyObj<PsApi>;
     component = fixture.componentInstance;
+    component.formGroup = new FormGroup({
+      [component['ID_NAT_PS']]: new FormControl('', Validators.required)
+    });
+    component.unsub$ = new Subject<void>();
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    component.unsub$.next();
+    component.unsub$.complete();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to form control value changes and reset errors when value is valid', () => {
+    const idNatPsControl = component.formGroup.get(component['ID_NAT_PS']);
+    idNatPsControl!.setErrors({ required: true });
+
+    spyOn(idNatPsControl!.valueChanges, 'pipe').and.callThrough();
+    spyOn(idNatPsControl!, 'setErrors').and.callThrough();
+
+    component.ngOnInit();
+
+    idNatPsControl!.setValue('valid value');
+    expect(idNatPsControl!.setErrors).toHaveBeenCalledWith(null);
+    expect(idNatPsControl?.errors).toBeNull();
+
+    idNatPsControl!.setValue('');
+    expect(idNatPsControl?.errors).not.toBeNull();
+  });
+
+  it('should set shouldShowAlert to true if form is invalid', () => {
+    component.formGroup.controls[component['ID_NAT_PS']].setValue('');
+    component.findPSByIDNat();
+
+    expect(component.isInvalid).toBe(true);
+    expect(component.shouldShowAlert).toBe(true);
+  });
+
+  it('should call getPSByIDNat if form is valid', () => {
+    const mockResponse = { status: QueryStatusEnum.OK, data: {} };
+    psApiService.getPSByIDNat.and.returnValue(of(mockResponse));
+
+    component.formGroup.controls[component['ID_NAT_PS']].setValue('validID');
+    component.findPSByIDNat();
+
+    expect(psApiService.getPSByIDNat).toHaveBeenCalledWith('validID');
+  });
+
+  it('should set response if API response status is OK', () => {
+    const mockResponse = { status: QueryStatusEnum.OK, data: { key: 'value' } };
+    psApiService.getPSByIDNat.and.returnValue(of(mockResponse));
+
+    component.formGroup.controls[component['ID_NAT_PS']].setValue('validID');
+    component.findPSByIDNat();
+
+    expect(component.response).toEqual({ data: mockResponse.data });
+  });
+
+  it('should set apiErrorMessage and shouldShowAlert if API response status is KO', () => {
+    const mockResponse = { status: QueryStatusEnum.KO, message: 'Error occurred' };
+    psApiService.getPSByIDNat.and.returnValue(of(mockResponse));
+
+    component.formGroup.controls[component['ID_NAT_PS']].setValue('validID');
+    component.findPSByIDNat();
+
+    expect(component.apiErrorMessage).toBe('Error occurred');
+    expect(component.shouldShowAlert).toBe(true);
+  });
+
+  it('should set default error message if API response status is KO and message is null', () => {
+    const mockResponse = { status: QueryStatusEnum.KO, message: null };
+    psApiService.getPSByIDNat.and.returnValue(of(mockResponse));
+
+    component.formGroup.controls[component['ID_NAT_PS']].setValue('validID');
+    component.findPSByIDNat();
+
+    expect(component.apiErrorMessage).toBe('Une erreur est survenue');
+    expect(component.shouldShowAlert).toBe(true);
   });
 });
